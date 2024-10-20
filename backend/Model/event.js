b5ef412d044dfc7613e87a909293ee75a708db71
ashikvincent07 <126868920+ashikvincent07@users.noreply.github.com>
2024-10-20 14:46:40 +0530

    Delete backend directory

--- backend/Model/event.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const eventSchema = new mongoose.Schema({
-    eventName: { type: String, required: true },
-    description: { type: String, required: true },
-    photo: { type: String, required: true } // Ensure this matches your schema
-});
-
-module.exports = mongoose.model('Event', eventSchema);
